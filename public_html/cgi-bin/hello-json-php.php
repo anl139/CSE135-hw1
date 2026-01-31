ade0dc7513fad2f0373bbef32da1014266366ce6
Andrew Lam <anl139@ucsd.edu>
2026-01-31 13:26:17 -0800

    Create hello-json-php.php

---
+++ public_html/cgi-bin/hello-json-php.php
@@ -0,0 +1,16 @@
+<?php
+header("Cache-Control: no-cache");
+header("Content-Type: application/json");
+
+$date = date("r");
+$ip = $_SERVER['REMOTE_ADDR'];
+
+$message = [
+    "title"   => "Hello, PHP!",
+    "heading" => "Hello, PHP!",
+    "message" => "This page was generated with the PHP programming language",
+    "time"    => $date,
+    "IP"      => $ip
+];
+
+echo json_encode($message, JSON_PRETTY_PRINT);
