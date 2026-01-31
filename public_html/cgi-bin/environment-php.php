f82728091973a91966541f23cb008be02fc4fa4e
Andrew Lam <anl139@ucsd.edu>
2026-01-31 13:27:11 -0800

    Create environment-php.php

---
+++ public_html/cgi-bin/environment-php.php
@@ -0,0 +1,24 @@
+<?php
+header("Cache-Control: no-cache");
+header("Content-Type: text/html");
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Environment Variables</title>
+</head>
+<body>
+
+<h1 align="center">Environment Variables</h1>
+<hr>
+
+<?php
+ksort($_SERVER);
+foreach ($_SERVER as $key => $value) {
+    echo "<b>" . htmlspecialchars($key) . ":</b> " . htmlspecialchars($value) . "<br />\n";
+}
+?>
+
+</body>
+</html>
