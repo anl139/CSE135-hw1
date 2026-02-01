2cc48b22fe7b46aa0e341c4a6cd718c02868f128
Andrew Lam <anl139@ucsd.edu>
2026-01-31 20:23:58 -0800

    Create hello-json-python.py

---
+++ public_html/cgi-bin/hello-json-python.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import os, sys, datetime, cgi, json, html
+now = datetime.datetime.utcnow().isoformat()+'Z'
+ip = os.environ.get('REMOTE_ADDR','unknown')
+form = cgi.FieldStorage()
+name = form.getfirst('name','Team')
+message = form.getfirst('message','')
+print("Content-Type: application/json; charset=utf-8")
+print()
+print(json.dumps({
+  "greeting": f"Hello from {name}",
+  "language": "Python (CGI)",
+  "message": message,
+  "generated": now,
+  "ip": ip
+}, indent=2))
