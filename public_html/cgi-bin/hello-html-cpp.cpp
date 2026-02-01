866c059052af0d8450f80813280ce21621a72799
Andrew Lam <anl139@ucsd.edu>
2026-02-01 15:42:08 -0800

    Update hello-html-cpp.cpp

--- public_html/cgi-bin/hello-html-cpp.cpp
+++ public_html/cgi-bin/hello-html-cpp.cpp
@@ -9,7 +9,7 @@ int main(){
   const char* ip = std::getenv("REMOTE_ADDR");
   if(!ip) ip = "unknown";
   std::cout << "Content-Type: text/html; charset=utf-8\r\n\r\n";
-  std::cout << "<!doctype html><html><head><meta charset='utf-8'><title>Hello C++</title></head><body>\n";
+  std::cout << "<!doctype html><html><head><meta charset='utf-8'><title>Hello Andrew Lam</title></head><body>\n";
   std::cout << "<h1>Hello Andrew Lam</h1>\n";
   std::cout << "<p>Generated: " << buf << "</p>\n";
   std::cout << "<p>Your IP: " << ip << "</p>\n";
