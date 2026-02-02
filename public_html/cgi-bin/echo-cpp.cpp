2bfb129aa5506d770429e02f08f626e7c385db8a
Andrew Lam <anl139@ucsd.edu>
2026-02-02 13:43:00 -0800

    Update echo-cpp.cpp

--- public_html/cgi-bin/echo-cpp.cpp
+++ public_html/cgi-bin/echo-cpp.cpp
@@ -1,4 +1,3 @@
-// echo-cpp.cpp
 #include <iostream>
 #include <cstdlib>
 #include <string>
@@ -6,120 +5,98 @@
 #include <map>
 #include <ctime>
 
-/* ---------------- Helper functions ---------------- */
-
-// Decode URL-encoded strings (for form data)
-std::string url_decode(const std::string& s) {
+/* ---------------- Helper ---------------- */
+auto url_decode = [](const std::string& s) {
     std::string out;
-    for (size_t i = 0; i < s.length(); i++) {
-        if (s[i] == '+') {
-            out += ' ';
-        } else if (s[i] == '%' && i + 2 < s.length()) {
-            std::string hex = s.substr(i + 1, 2);
-            out += static_cast<char>(std::strtol(hex.c_str(), nullptr, 16));
-            i += 2;
-        } else {
-            out += s[i];
-        }
+    for (size_t i = 0; i < s.size(); ++i) {
+        if (s[i] == '+') out += ' ';
+        else if (s[i] == '%' && i + 2 < s.size())
+            out += static_cast<char>(std::strtol(s.substr(i+1,2).c_str(), nullptr, 16)), i+=2;
+        else out += s[i];
     }
     return out;
-}
+};
 
-// Parse application/x-www-form-urlencoded body
-std::map<std::string, std::string> parse_form(const std::string& body) {
-    std::map<std::string, std::string> data;
-    std::stringstream ss(body);
-    std::string pair;
-
-    while (std::getline(ss, pair, '&')) {
-        size_t eq = pair.find('=');
-        if (eq != std::string::npos) {
-            std::string key = url_decode(pair.substr(0, eq));
-            std::string val = url_decode(pair.substr(eq + 1));
-            data[key] = val;
-        }
+auto parse_form = [&](const std::string& body) {
+    std::map<std::string,std::string> data;
+    std::stringstream ss(body); std::string pair;
+    while (std::getline(ss,pair,'&')) {
+        auto eq = pair.find('=');
+        if (eq != std::string::npos)
+            data[url_decode(pair.substr(0,eq))] = url_decode(pair.substr(eq+1));
     }
     return data;
-}
+};
 
-// Escape strings for safe JSON output
-std::string json_escape(const std::string& s) {
+auto json_escape = [](const std::string& s) {
     std::ostringstream o;
-    for (char c : s) {
-        switch (c) {
-            case '"':  o << "\\\""; break;
-            case '\\': o << "\\\\"; break;
-            case '\n': o << "\\n";  break;
-            case '\r': o << "\\r";  break;
-            case '\t': o << "\\t";  break;
-            default:   o << c;
-        }
+    for (char c : s) switch(c){
+        case '"': o<<"\\\""; break;
+        case '\\': o<<"\\\\"; break;
+        case '\n': o<<"\\n"; break;
+        case '\r': o<<"\\r"; break;
+        case '\t': o<<"\\t"; break;
+        default: o<<c;
     }
     return o.str();
-}
+};
 
-/* ---------------- Main CGI program ---------------- */
-
+/* ---------------- Main ---------------- */
 int main() {
-    const char* method       = std::getenv("REQUEST_METHOD");
-    const char* content_type = std::getenv("CONTENT_TYPE");
-    const char* host         = std::getenv("HTTP_HOST");
-    const char* ua           = std::getenv("HTTP_USER_AGENT");
-    const char* ip           = std::getenv("REMOTE_ADDR");
+    const char* method = std::getenv("REQUEST_METHOD") ?: "GET";
+    const char* content_type = std::getenv("CONTENT_TYPE") ?: "";
+    const char* host = std::getenv("HTTP_HOST") ?: "";
+    const char* ua = std::getenv("HTTP_USER_AGENT") ?: "";
+    const char* ip = std::getenv("REMOTE_ADDR") ?: "unknown";
 
-    if (!method) method = "GET";
-    if (!content_type) content_type = "";
-    if (!ip) ip = "unknown";
-
-    // Read request body (if present)
+    // Read request body
     std::string body;
-    const char* len_s = std::getenv("CONTENT_LENGTH");
-    if (len_s) {
+    if (const char* len_s = std::getenv("CONTENT_LENGTH")) {
         int len = std::atoi(len_s);
         body.resize(len);
         std::cin.read(&body[0], len);
     }
 
-    // Parsed data
-    std::map<std::string, std::string> data;
+    std::map<std::string,std::string> data;
+    std::string format;
+    std::string ctype(content_type);
 
-    if (std::string(content_type).find("application/x-www-form-urlencoded") != std::string::npos) {
+    if (ctype.find("application/x-www-form-urlencoded") != std::string::npos) {
         data = parse_form(body);
+        format = "www-form";
     }
-    else if (std::string(content_type).find("application/json") != std::string::npos) {
-        // No JSON parser used (acceptable for HW)
+    else if (ctype.find("application/json") != std::string::npos) {
         data["__json"] = body;
+        format = "json";
+    } else {
+        format = "other";
     }
 
     // Timestamp
-    std::time_t t = std::time(nullptr);
     char timebuf[64];
-    std::strftime(timebuf, sizeof(timebuf),
-                  "%Y-%m-%dT%H:%M:%SZ",
-                  std::gmtime(&t));
+    std::strftime(timebuf, sizeof(timebuf), "%Y-%m-%dT%H:%M:%SZ", std::gmtime(std::time(nullptr)));
 
-    // Output response
+    // Output JSON
     std::cout << "Content-Type: application/json; charset=utf-8\r\n\r\n";
-    std::cout << "{\n";
-    std::cout << "  \"method\": \"" << method << "\",\n";
-    std::cout << "  \"host\": \"" << (host ? host : "") << "\",\n";
-    std::cout << "  \"time\": \"" << timebuf << "\",\n";
-    std::cout << "  \"ip\": \"" << ip << "\",\n";
-    std::cout << "  \"user_agent\": \"" << json_escape(ua ? ua : "") << "\",\n";
-    std::cout << "  \"content_type\": \"" << content_type << "\",\n";
-    std::cout << "  \"raw_body\": \"" << json_escape(body) << "\",\n";
-    std::cout << "  \"data\": {\n";
+    std::cout << "{\n"
+              << "  \"method\": \"" << method << "\",\n"
+              << "  \"host\": \"" << host << "\",\n"
+              << "  \"time\": \"" << timebuf << "\",\n"
+              << "  \"ip\": \"" << ip << "\",\n"
+              << "  \"user_agent\": \"" << json_escape(ua) << "\",\n"
+              << "  \"content_type\": \"" << content_type << "\",\n"
+              << "  \"format\": \"" << format << "\",\n"
+              << "  \"raw_body\": \"" << json_escape(body) << "\",\n"
+              << "  \"data\": {\n";
 
     bool first = true;
-    for (const auto& kv : data) {
+    for (auto& [k,v] : data) {
         if (!first) std::cout << ",\n";
         first = false;
-        std::cout << "    \"" << json_escape(kv.first) << "\": \""
-                  << json_escape(kv.second) << "\"";
+        std::cout << "    \"" << json_escape(k) << "\": \"" << json_escape(v) << "\"";
     }
-
-    std::cout << "\n  }\n";
-    std::cout << "}\n";
+    std::cout << "\n  }\n}\n";
 
     return 0;
 }
+
