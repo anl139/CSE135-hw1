067dfdc42c4755a6e3066ee6f0bf2415b7a0ecce
Andrew Lam <anl139@ucsd.edu>
2026-02-03 11:15:37 -0800

    Update perl-sessions-1.pl

--- public_html/cgi-bin/perl-sessions-1.pl
+++ public_html/cgi-bin/perl-sessions-1.pl
@@ -1,40 +1,52 @@
 #!/usr/bin/perl
+use strict;
+use warnings;
 use CGI;
-
-# Create a new Perl Session
+use CGI::Carp qw(fatalsToBrowser);
 use CGI::Session;
-$session = new CGI::Session("driver:File", undef, {Directory=>"/tmp"});
 
-# Create CGI Object
-$cgi = CGI->new;
+my $cgi = CGI->new;
 
-# Create a new Cookie from the Session ID
-$cookie = $cgi->cookie(CGISESSID => $session->id);
-print $cgi->header( -cookie=>$cookie );
+my $session = CGI::Session->new(
+    "driver:File",
+    undef,
+    { Directory => "/tmp" }
+);
 
-#Store Data in that Perl Session
+my $cookie = $cgi->cookie(
+    -name  => 'CGISESSID',
+    -value => $session->id
+);
+
+print $cgi->header( -cookie => $cookie );
+
 my $name = $session->param('username') || $cgi->param('username');
-$session->param("username", $name);
+$session->param( "username", $name );
 
-print "<html>";
-print "<head>";
-print "<title>Perl Sessions</title>";
-print "</head>";
-print "<body>";
+print <<HTML;
+<html>
+<head><title>Perl Sessions</title></head>
+<body>
+<h1>Perl Sessions Page 1</h1>
+HTML
 
-print "<h1>Perl Sessions Page 1</h1>";
-
-if ($name){
-	print("<p><b>Name:</b> $name");
-}else{
-	print "<p><b>Name:</b> You do not have a name set</p>";
+if ($name) {
+    print "<p><b>Name:</b> $name</p>";
+} else {
+    print "<p><b>Name:</b> You do not have a name set</p>";
 }
-print "<br/><br/>";
-print "<a href=\"/cgi-bin/perl-sessions-2.pl\">Session Page 2</a><br/>";
-print "<a href=\"/perl-cgiform.html\">Perl CGI Form</a><br />";
-print "<form style=\"margin-top:30px\" action=\"/cgi-bin/perl-destroy-session.pl\" method=\"get\">";
-print "<button type=\"submit\">Destroy Session</button>";
-print "</form>";
+
+print <<HTML;
+<br/><br/>
+<a href="/cgi-bin/perl-sessions-2.pl">Session Page 2</a><br/>
+<a href="/perl-cgiform.html">Perl CGI Form</a><br/>
+<form style="margin-top:30px" action="/cgi-bin/perl-destroy-session.pl" method="get">
+<button type="submit">Destroy Session</button>
+</form>
+</body>
+</html>
+HTML
+
 
 print "</body>";
 print "</html>";
