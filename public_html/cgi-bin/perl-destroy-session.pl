71b261cc5267d7e98d26ebd05806c6c130e5649a
Andrew Lam <anl139@ucsd.edu>
2026-02-03 11:18:36 -0800

    Update perl-destroy-session.pl

--- public_html/cgi-bin/perl-destroy-session.pl
+++ public_html/cgi-bin/perl-destroy-session.pl
@@ -1,26 +1,43 @@
 #!/usr/bin/perl
-use CGI::Session;
+use strict;
+use warnings;
 use CGI;
+use CGI::Session;
+use CGI::Carp qw(fatalsToBrowser);
 
-print "Content-type: text/html\n\n";
-
-my $cgi = new CGI;
+my $cgi = CGI->new;
+my $sid = $cgi->cookie('CGISESSID');
 
 my $session;
-{
-    my $sid = $cgi->cookie("SITE_SID") || $cgi->param("sid") || undef;
-    $session  = new CGI::Session($sid);
+if ($sid) {
+    $session = CGI::Session->new(
+        undef,
+        $sid,
+        { Directory => '/tmp' }
+    );
+    $session->delete;
 }
-$session->delete();
 
-print "<html>";
-print "<head>";
-print "<title>Perl Session Destroyed</title>";
-print "</head>";
-print "<body>";
-print "<h1>Session Destroyed</h1>";
-print "<a href=\"/perl-cgiform.html\">Back to the Perl CGI Form</a><br />";
-print "<a href=\"/cgi-bin/perl-sessions-1.pl\">Back to Page 1</a><br />";
-print "<a href=\"/cgi-bin/perl-sessions-2.pl\">Back to Page 2</a>";
-print "</body>";
-print "</html>";
+# Expire cookie in browser
+my $expired_cookie = $cgi->cookie(
+    -name    => 'CGISESSID',
+    -value   => '',
+    -expires => '-1d'
+);
+
+print $cgi->header(
+    -type   => 'text/html',
+    -cookie => $expired_cookie
+);
+
+print <<HTML;
+<html>
+<head><title>Perl Session Destroyed</title></head>
+<body>
+<h1>Session Destroyed</h1>
+<a href="/perl-cgiform.html">Back to the Perl CGI Form</a><br />
+<a href="/cgi-bin/perl-sessions-1.pl">Back to Page 1</a><br />
+<a href="/cgi-bin/perl-sessions-2.pl">Back to Page 2</a>
+</body>
+</html>
+HTML
