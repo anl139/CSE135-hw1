572c613902da19f21e26e7c7d34ec90f38507fa5
Andrew Lam <anl139@ucsd.edu>
2026-03-11 14:42:03 -0700

    Update auth.php

--- reporting/reporting.anl139.site/public_html/auth.php
+++ reporting/reporting.anl139.site/public_html/auth.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-// Call this at the top of every protected page
+// Call this at the top of every protected pagesadfsdfa
 function require_auth() {
     if (!isset($_SESSION['user'])) {
         header("Location: /login.php");
