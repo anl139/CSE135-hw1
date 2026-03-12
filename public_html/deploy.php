116dcda740dbf1a21176797863d4be9561ab390c
Andrew Lam <anl139@ucsd.edu>
2026-03-11 18:25:28 -0700

    Update deploy.php

--- public_html/deploy.php
+++ public_html/deploy.php
@@ -5,25 +5,28 @@
  * Automatically deploy all folders in the repo via GitHub
  */
 
+// Base directory of your repo
+$baseDir = '/var/www/anl139.site';
+
 // List of folders to deploy
 $folders = ['public_html', 'collector', 'reporting', 'experiment'];
 
 $output = '';
 
 foreach ($folders as $folder) {
+    $path = "$baseDir/$folder"; // absolute path to folder
+
     $commands = [
         "echo 'Deploying $folder...'",
-        "cd $folder",
-        "git pull",
-        "git status",
-        "git submodule sync",
-        "git submodule update",
-        "git submodule status",
-        "cd .."
+        "cd $path && git pull",
+        "cd $path && git status",
+        "cd $path && git submodule sync",
+        "cd $path && git submodule update",
+        "cd $path && git submodule status",
     ];
 
     foreach ($commands as $command) {
-        $tmp = shell_exec($command);
+        $tmp = shell_exec($command . ' 2>&1'); // capture stderr too
         $output .= "<span style=\"color: #6BE234;\">\$</span><span style=\"color: #729FCF;\"> {$command}</span><br />";
         $output .= htmlentities(trim($tmp)) . "<br /><br />";
     }
@@ -39,7 +42,5 @@ foreach ($folders as $folder) {
 <body style="background-color: #000; color: #fff; font-weight: bold; padding: 10px;">
     <h2>Git Deployment Script Output</h2>
     <?php echo $output; ?>
-</body>
-</html>
 </body>
 </html>
