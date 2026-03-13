9a24125226667e68d844e252b35920cc5fb1313c
Andrew Lam <anl139@ucsd.edu>
2026-03-13 14:35:37 -0700

    Update reports.php

--- reporting/reporting.anl139.site/public_html/reports.php
+++ reporting/reporting.anl139.site/public_html/reports.php
@@ -425,8 +425,8 @@ $analystComments = [
 
 <script type="application/json" id="dashboard-data">
 <?= json_encode([
-    'activityCounts' => array_values($activityCountsChart),
-    'navTiming' => $navTimingChart
+    'activitySeries' => $activitySeries,
+    'performanceSeries' => $performanceSeries,
 ], JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_AMP | JSON_HEX_QUOT) ?>
 </script>
 
