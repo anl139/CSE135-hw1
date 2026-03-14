97bfe7ffbb493b2fa2c3133ca7ec2bc01b195d72
Andrew Lam <anl139@ucsd.edu>
2026-03-14 13:11:47 -0700

    Update echo-python.py

--- public_html/cgi-bin/echo-python.py
+++ public_html/cgi-bin/echo-python.py
@@ -2,21 +2,14 @@
 import os, sys, json, datetime
 from urllib.parse import parse_qs
 
-# Get HTTP method
 method = os.environ.get('REQUEST_METHOD', 'GET')
-
-# Read request body if applicable
 raw_body = ''
 if method in ['POST', 'PUT', 'DELETE']:
-    length = int(os.environ.get('CONTENT_LENGTH', 0) or 0)
-    if length:
-        raw_body = sys.stdin.read(length)
+    raw_body = sys.stdin.read()
 
-# Parse incoming data
 content_type = os.environ.get('CONTENT_TYPE','').lower()
 data = {}
-content_format = 'none'
-
+content_format = 'none'  # default
 if method == 'GET':
     qs = os.environ.get('QUERY_STRING','')
     data = {k: v[0] for k,v in parse_qs(qs).items()}
@@ -31,11 +24,11 @@ elif 'application/x-www-form-urlencoded' in content_type:
     data = {k: v[0] for k,v in parse_qs(raw_body).items()}
     content_format = 'www-form'
 else:
+    # unknown or other content types
     data = {"raw": raw_body}
     content_format = 'other'
 
-# Build response
-now = datetime.datetime.utcnow().isoformat() + 'Z'
+now = datetime.datetime.utcnow().isoformat()+'Z'
 resp = {
     "method": method,
     "host": os.environ.get('HTTP_HOST',''),
@@ -48,9 +41,6 @@ resp = {
     "raw_body": raw_body
 }
 
-# Send response
 print("Content-Type: application/json; charset=utf-8")
 print()
 print(json.dumps(resp, indent=2))
-
-
